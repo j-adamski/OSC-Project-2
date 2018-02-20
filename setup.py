a6426394dfaf914449af9d0d9d62270a2029cea7
thatzopoulos <thatzopoulos965@gmail.com>
2018-02-19 23:31:13 -0600

    barebones test setup and t3 code that just prints testing

---
+++ setup.py
@@ -0,0 +1,22 @@
+try:
+	from setuptools import setup
+except:
+	from distutils.core import setup
+
+setup(
+	name='t3',
+	version="0.10",
+	author="comp412GroupProject",
+	author_email='thatzopoulos@luc.edu',
+	url='https://github.com/j-adamski/OSC-Project-2/',
+        licence='GPL',
+	py_modules=['t3'],
+	entry_points={
+		'console_scripts': [
+			't3 = t3:_main',
+			],
+	},
+
+)
+
+#TO RUN: pip install .
---
+++ t3.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import argparse
+
+"""time tracker and to do list, T3. Todo list where you can add your tasks, keep track of how long you have been working on them, and save your output to a text file for additional scripting"""
+
+"""T3 is to assist those who struggle with time management through a simple, easy to use to do list time tracker """
+
+import datetime
+import os
+
+#Commands to Implement
+
+#t3 (a)dd 'activity'
+	#This counts as a working time activity that will be saved under projects
+	#OPTIONAL: activities that end in 'break' are tagged as break time, allows you to graph time wasted
+#t3 (d)elete 'activity'
+	#removes activity when finished
+	#TODO: should remove and mark as completed be separate things?
+#t3 (r)eport 'date'
+	#print out basic report of the day entered. Default is a week. #TODO: need to decide on a date format
+	#Report is just a clean printout of the tasks and times, can be extended after the fact for graphs and html docs
+
+#Starting and stopping timing
+#t3 (o)n/start 'setup project'
+	#starts/adds to time of project, making it the current tracked activity.
+	#OPTIONAL: be able to say how long ago you started the activity so it can add that to the current tracked time
+#t3 (f)inish/stop 'setup project'
+	#saves time of current activity and stops timing it
+	#TODO: finish can be swapped to stop or something else
+
+#t3 help 
+	#prints out help dialog
+
+#adding multiple tasks under one project:
+	#Parse any non whitespace string followed by a clon as a project, anything after it is a task within that project
+	#t3 add "project1: task four"
+	#t3 add "project1: task two"
+	#t3 report
+	#------- Projects --------
+	#(0h30m) : project1: task four, task two
+
+def _main():
+	"""Run the CLI Interface for T3"""
+	print('testing')
+
+if __name__ == '__main__':
+	_main()
