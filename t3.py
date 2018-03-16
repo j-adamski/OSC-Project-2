fde62390c87819151ea2d7145c4758bf91fa4534
thatzopoulos <thatzopoulos965@gmail.com>
2018-03-15 20:34:28 -0500

    the add function currently works. delete and edit were started but do not currently work. Delete and edit still write to test outputs while they are being fixed. if someone knows what is up with delete, let me know.

--- t3.py
+++ t3.py
@@ -43,7 +43,7 @@ import json
 	#(0h30m) : project1: task four, task two
 
 def t3Report():
-    inputFile = 'tasks/exampleTasks.txt'
+    inputFile = 'tasks/exampleTasks.json'
     jsonData=open(inputFile).read()
     jsonToPython = json.loads(jsonData)
 
@@ -53,17 +53,58 @@ def t3Report():
     print(json.dumps(jsonToPython, indent=4, sort_keys=True))
 
 
+def t3Add():
+    inputFile = 'tasks/exampleTasks.json'
+    jsonData=open(inputFile).read()
+    jsonToPython = json.loads(jsonData)
+    
+    projectName = input("project name: ")
+    
+    #TODO: add task functionality following the above guidelines
+    #taskName = input("task name: ")
+    add_dict = {projectName: [{
+        "taskName": "","timeSpent":0,"completed":False}]}
+    jsonToPython.update(add_dict)
+    
+    #test file to write to while testing
+    #with open('tasks/test.json', 'w') as f:
+    with open('tasks/exampleTasks.json', 'w') as f:
+        json.dump(jsonToPython,f)
+def t3Delete():
+    #TODO Doesn't currently run
+    print("For testing use project1 as project name")
+    projectName = input("project name to delete: ")
+    with open('tasks/testDelete.json', 'w') as dest_file:
+        with open('tasks/exampleTasks.json', 'r') as source_file:
+            for line in source_file:
+                element = json.loads(line.strip())
+                if projectName in element:
+                    del element[projectName]
+                dest_file.write(json.dumps(element))
+def t3Edit():
+    #TODO the ability to edit the time of a project
+    print("Not implemented")
+    print("edits the time spent doing the activity")
+    inputFile = 'tasks/exampleTasks.json'
+    jsonData=open(inputFile).read()
+    jsonToPython = json.loads(jsonData)
+    #projectName = input("Project Name To Edit Time Of: ")
+    
+
+
+
 def _main():
     """Run the CLI Interface for T3"""
 
     #parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--add", help="Counts as a working time activity, will be saved under Projects in exampleTimedTasks")
-    parser.add_argument("--delete", help="removes activity when finished from exampleTimedTasks")
+    parser.add_argument("--add", help="Counts as a working time activity, will be saved under Projects in exampleTimedTasks",action='store_true', default=False, dest='addValue')
+    parser.add_argument("--delete", help="removes activity when finished from exampleTimedTasks",action='store_true',default=False,dest='deleteValue')
     parser.add_argument("--report", help="prints out basic report of the week by default from exampleTimedTasks",action='store_true', default=False, dest='reportValue',)
     parser.add_argument("--start/on", help="starts,adds time to project, making it the current tracked activity")
     parser.add_argument("--stop/finish", help="stops timing of project, removes it as the currect tracked activity")
     parser.add_argument("--break", help="adds time spent on break to exampleTimedTasks")
+    parser.add_argument("--edit", help="edits the time spent on a project", action='store_true',default=False,dest="editValue")
     results = parser.parse_args()
     print('Run Program as python3 t3.py --help for descriptions of arguments')
     print('\n')
@@ -72,6 +113,13 @@ def _main():
 
     if (results.reportValue) == True:
         t3Report()
+    if (results.addValue) == True:
+        t3Add()
+    if (results.deleteValue) == True:
+        t3Delete()
+    if (results.editValue) == True:
+        t3Edit()
+    
     #t3Report()
         #t3Report('exampleTasks.txt')
 if __name__ == '__main__':
