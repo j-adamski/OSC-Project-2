c24461b4803ee1d3d1a31e5271566baa50a061f1
Prachip25 <36549726+Prachip25@users.noreply.github.com>
2018-04-21 15:27:51 -0500

    Update logo.py

--- logo.py
+++ logo.py
@@ -4,37 +4,30 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.tri as mtri
 
 fig = plt.figure(figsize=plt.figaspect(0.5))
-
-
-
-
 n_angles = 36
 n_radii = 8
 min_radius = 0.25
 radii = np.linspace(min_radius, 0.95, n_radii)
-
 angles = np.linspace(0, 2*np.pi, n_angles, endpoint=False)
 angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
 angles[:, 1::2] += np.pi/n_angles
-
 x = (radii*np.cos(angles)).flatten()
 y = (radii*np.sin(angles)).flatten()
 z = (np.cos(radii)*np.cos(angles*3.0)).flatten()
-
 triang = mtri.Triangulation(x, y)
-
 xmid = x[triang.triangles].mean(axis=1)
 ymid = y[triang.triangles].mean(axis=1)
 mask = np.where(xmid**2 + ymid**2 < min_radius**2, 1, 0)
 triang.set_mask(mask)
-
-
 ax = fig.add_subplot(1, 2, 2, projection='3d')
 ax.plot_trisurf(triang, z, cmap=plt.cm.CMRmap)
-plt.title("TODO")
+fig.suptitle('TODO', fontsize=50, fontweight='ultralight')
+
+
 plt.xticks([])
 plt.yticks([])
-
-
-
+ax.w_zaxis.line.set_lw(0.)
+ax.set_zticks([])
 plt.show()
+
+
